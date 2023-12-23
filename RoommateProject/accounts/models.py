082749cb8d997f0812694796f103125b2c0f3db1
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-12-24 01:55:56 +0300

    ok

--- RoommateProject/accounts/models.py
+++ RoommateProject/accounts/models.py
@@ -1,7 +1,47 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from advertisements.models import Advertisement
+from django_countries.fields import CountryField
 # Create your models here.
 
 
 
+
+class Profile(models.Model):
+
+    genders = models.TextChoices("genders" , ["male","fmmale"])
+    languages = models.TextChoices ("languages" , ["einglish","arabic"])
  
+
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    personal_image = models.ImageField(upload_to="images/", default="images/ge.jpg")
+    phone_number = models.CharField(max_length=64)
+    gender = models.CharField( max_length=64, choices=genders.choices , default=genders.choices)
+    age = models.IntegerField()
+    about= models.TextField()
+    language = models.CharField( max_length=64, choices=languages.choices , default=languages.choices )
+    nationality = CountryField()
+
+    def __str__(self):
+        return f"{self.user.first_name} profile"
+
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(User ,on_delete=models.CASCADE)
+    advertisement = models.ForeignKey(Advertisement, on_delete=models.CASCADE)
+
+
+
+class Validation(models.Model):
+    user = models.OneToOneField(User ,on_delete=models.CASCADE)
+    national_id = models.CharField(max_length=64)  
+    id_image = models.ImageField(upload_to="images/")
+    validated = models.BooleanField(default=False)
+
+
+
+
+
+
+    
--- RoommateProject/accounts/urls.py
+++ RoommateProject/accounts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from . import views
 app_name = "accounts"
+
 urlpatterns = [
 
      path("register/", views.register_user_view, name="register_user_view"),
      path("login/", views.login_user_view, name="login_user_view"),
      path("logout/", views.logout_user_view, name="logout_user_view"),
+     path("profile/<int:user_id>/", views.user_profile, name="user_profile"),
+     path("update/", views.update_user_view, name="update_user_view"),
+     path('<advertisement_id>/add/', views.add_favorite, name="add_favorite"),
+     path('favorite/', views.favorite, name="favorite"),
+
     
 ]
