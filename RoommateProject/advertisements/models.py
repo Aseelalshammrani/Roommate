1c1f5f7ec7ebcb4c89b5fa33d61f3cc1050f65e3
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-12-24 10:36:34 +0300

    done

--- RoommateProject/advertisements/models.py
+++ RoommateProject/advertisements/models.py
@@ -4,7 +4,7 @@ class Advertisement(models.Model):
     types_of_gender=models.TextChoices('types_of_gender',['Female','Male'])
     types_of_residential=models.TextChoices('types_of_residential',['Apartment','Room','House'])
     types_of_duration =models.TextChoices('types_of_duration',['Days','Weeks','Months'])
-    approval_status=models.TextChoices('approval_status',['Approved','Denied'])
+    approval_status=models.TextChoices('approval_status',['Pending','Approved','Denied'])
     title=models.CharField(max_length=500)
     image=models.ImageField(upload_to="images/")
     type_of_duration =models.CharField(max_length=500,choices=types_of_duration.choices)
@@ -27,7 +27,7 @@ class Advertisement(models.Model):
     rooms_number=models.IntegerField()
     bathroom=models.IntegerField()
     has_kitchen=models.BooleanField(default=False)
-    approved_status=models.CharField(max_length=500,default='pending',choices=approval_status.choices)
+    approved_status=models.CharField(max_length=500,default='Pending',choices=approval_status.choices)
 
 class Advertisement_Image(models.Model):
     advertisement=models.ForeignKey(Advertisement,on_delete=models.CASCADE)
