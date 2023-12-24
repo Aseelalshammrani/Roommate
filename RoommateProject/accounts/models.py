f2fc3ac5544671a8926072783d113b130a6bc4f1
MaramAlasmari <147453012+MaramAlasmari@users.noreply.github.com>
2023-12-24 10:47:52 +0300

    done

--- RoommateProject/accounts/models.py
+++ RoommateProject/accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from advertisements.models import Advertisement
-from django_countries.fields import CountryField
+
 # Create your models here.
 
 
