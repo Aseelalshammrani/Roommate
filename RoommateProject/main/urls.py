0f49f26a2945f342d8a4650522383b3260daef21
Sufana_Aljawi <Sufanaaljawi@gmail.com>
2023-12-27 00:35:58 +0300

    Add favorite function

--- RoommateProject/main/urls.py
+++ RoommateProject/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 app_name = "main"
 urlpatterns = [
-    path('',views.home_page,name='home_page')
+    path('',views.home_page,name='home_page'),
+    path('approve/', views.approve_accounts_view, name="approve_accounts_view")
 ]
