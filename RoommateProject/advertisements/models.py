a0187293087b1e6b300703cb39fcc5f09a4e0eab
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-12-28 10:48:06 +0300

    Add Review model

--- RoommateProject/advertisements/models.py
+++ RoommateProject/advertisements/models.py
@@ -51,6 +51,16 @@ class Rent_Request(models.Model):
     order_status_choice = models.CharField(max_length=500,default='Pending',choices=order_status.choices)
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Review(models.Model):
+    user = models.ForeignKey(User ,on_delete=models.CASCADE)
+    advertisement=models.ForeignKey(Advertisement,on_delete=models.CASCADE)
+    content = models.TextField()
+    rating = models.IntegerField()
+    created_at= models.DateField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.user} : {self.content}"
+
  
 
 
